194b8a36df361465465ce387bccf82854ce888a4
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2024-03-14 16:28:17 +0000

    NPC fear timer bugfix, camera lerp, & commenting
    - got rid of potential bug where if the NPC's fear timer ran out, they would stop running while the player re-entered their range.
    
    - Added lerp to camera, hopefully it will be smooth as this time I'm not adding forces but changing velocity unlike last game's controller.

--- Assets/Scripts/NPCs/BasicNPCBehaviourScript.cs
+++ Assets/Scripts/NPCs/BasicNPCBehaviourScript.cs
@@ -7,18 +7,23 @@ public class BasicNPCBehaviourScript : MonoBehaviour
     public Rigidbody player;
 
     private Rigidbody body;
-    private Vector3 runDirection;
-    private float speedMultiplier;
-    private float fearRunningTime = 5.0f;
-    private bool playerSeen = false;
-    private bool playerInRange = false;
-    private bool caught = false;
+    private Vector3 runDirection;        //direction which the NPC runs away in.
+    private float speedMultiplier;       //how fast the NPC runs, relative to speed of player.
+    private float fearRunningTime = 5.0f;//how long the NPC runs away after they spotted the player
+    private bool playerSeen = false;     //has the NPC seen the player?
+    private bool playerInRange = false;  //is the player inside of the visibility collider?
+    private bool caught = false;         //was the NPC caught by the player?
 
 
     IEnumerator RunningInFear()
     {
         yield return new WaitForSeconds(fearRunningTime);
-        playerSeen = false;
+        
+        if (!playerInRange)//inRange check prevents potential bug where NPC stops if player re-enters collision range before fear running is complete.
+        {
+            playerSeen = false;
+        }
+        
     }
 
 
@@ -45,8 +50,9 @@ public class BasicNPCBehaviourScript : MonoBehaviour
             transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f)); //face away from player
         }
 
-        if (playerSeen && !caught)
+        if ((playerSeen) && !caught)
         {
+            
             //run in the last known run direction
             body.velocity = runDirection * speedMultiplier;
         }
