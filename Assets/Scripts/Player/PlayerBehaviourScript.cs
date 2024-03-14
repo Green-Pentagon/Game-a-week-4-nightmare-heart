e40833cb2c2e1085b53596dbd2a4b5fb4ba44622
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2024-03-14 15:15:03 +0000

    basic movement get

--- Assets/Scripts/Player/PlayerBehaviourScript.cs
+++ Assets/Scripts/Player/PlayerBehaviourScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerBehaviourScript : MonoBehaviour
 {
+    private Rigidbody body;
+    private float velocityMultiplier = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizInput = Input.GetAxis("Horizontal"); // -1, 0, or 1 depending on user input
+        float vertInput = Input.GetAxis("Vertical"); // -1, 0, or 1 depending on user input
+        
+        if (horizInput != vertInput)
+        {
+            body.velocity = new Vector3(horizInput * velocityMultiplier, body.velocity.y, vertInput * velocityMultiplier);
+        }
+       
         
     }
 }
