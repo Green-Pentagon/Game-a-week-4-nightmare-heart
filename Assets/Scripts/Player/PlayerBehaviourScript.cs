2b24900805bcb780dbe03e6c6f394ea84d1ce916
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2024-03-14 18:14:33 +0000

    Added Player Rotation + changes to Rigidbody
    - player now rotates in the direction of their velocity.
    - player's y position locked to prevent rotation bug.
    - player's rotation fully constricted (only can be changed by the scripts and not by collisions)
    - Added an optional rotation method where if the player is not moving, their character looks at cursor position (may be utilised later, for now its commented out.)

--- Assets/Scripts/Player/PlayerBehaviourScript.cs
+++ Assets/Scripts/Player/PlayerBehaviourScript.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerBehaviourScript : MonoBehaviour
 {
     public const float SPEED_MULTIPLIER = 5.0f;
+    public Camera Camera;
 
     private Rigidbody body;
-    
+    private Ray ray;
+    private RaycastHit hitPos;
+    private Vector3 LookAtPosition = Vector3.zero;
+    private bool hit;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,10 @@ public class PlayerBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(":3");
+        }
     }
 
     private void FixedUpdate()
@@ -26,9 +34,22 @@ public class PlayerBehaviourScript : MonoBehaviour
         float horizInput = Input.GetAxis("Horizontal"); // -1, 0, or 1 depending on user input
         float vertInput = Input.GetAxis("Vertical"); // -1, 0, or 1 depending on user input
         
-        if (horizInput != vertInput)
+        if (horizInput != 0 || vertInput != 0)
         {
+            //set velocity
             body.velocity = new Vector3(horizInput * SPEED_MULTIPLIER, body.velocity.y, vertInput * SPEED_MULTIPLIER);
+
+            //transform look in direction of velocity
+            transform.LookAt(transform.position + Vector3.Normalize(body.velocity));
+        }
+        else
+        {
+            ////looking around
+            //ray = Camera.ScreenPointToRay(Input.mousePosition);
+            //hit = Physics.Raycast(ray, out hitPos, 50.0f, 1 << LayerMask.NameToLayer("Ground"));
+
+            //LookAtPosition = new Vector3(hitPos.point.x, transform.position.y, hitPos.point.z);
+            //transform.LookAt(LookAtPosition);
         }
        
         
